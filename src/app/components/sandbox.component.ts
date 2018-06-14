3954d3a5bd91ddb7de58d03d4e463a9f6e04c447
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-06-14 16:30:11 +0530

    the appp working, till interface in same file
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -1,41 +1,53 @@
 import { Component } from '@angular/core';
 @Component({
     selector:'sandbox',
-    template:`<h1>
-                    {{name}} is {{age}} yrs old
-              </h1>
-              <h2>
-                    his name is {{person.firstname}} {{person.lastname}}
-              </h2>
-              <p>{{showAge()}}</p>
-    `
+    // template:`<h1>
+    //                 {{name}} is {{age}} yrs old
+    //           </h1>
+    //           <h2>
+    //                 his name is {{person.firstname}} {{person.lastname}}
+    //           </h2>
+    //           <p>{{showAge()}}</p>
+    // `
+    template:`<h1>hello world</h1>`
 })
 
 export class SandboxComponent{
-    name:string = "alabama";
-    age:number = 20;
-    person = {firstname:"Steven", lastname:"Tyler"};
-    hasChildren:boolean=true;
-    city:any = "Boston";//can be any type
-    myNumbersArray:number[] = [1,2,3];
-    myStringsarray:string[] =["helo","world"];
-    myAnyArray:any[] = [1,2,"Hello"];
-    myTuple:[string, number]=["hello",3];
-    unusable:void =undefined;
-    u:undefined = undefined;
-    n:null=null;
-    s:null=undefined;
+    // name:string = "alabama";
+    // age:number = 20;
+    // person = {firstname:"Steven", lastname:"Tyler"};
+    // hasChildren:boolean=true;
+    // city:any = "Boston";//can be any type
+    // myNumbersArray:number[] = [1,2,3];
+    // myStringsarray:string[] =["helo","world"];
+    // myAnyArray:any[] = [1,2,"Hello"];
+    // myTuple:[string, number]=["hello",3];
+    // unusable:void =undefined;
+    // u:undefined = undefined;
+    // n:null=null;
+    // s:null=undefined;
+    // constructor(){
+    //     console.log("running constructor");
+    //     //can directly initialise variable here,
+    //     //without even adding the property
+    //     //this.age=23;
+    //     this.hasBirthday();
+    // }
+    // hasBirthday(){
+    //     this.age+=1;
+    // }
+    // showAge(){
+    //     return this.age;
+    // }
+    customer:Customer;
     constructor(){
-        console.log("running constructor");
-        //can directly initialise variable here,
-        //without even adding the property
-        //this.age=23;
-        this.hasBirthday();
+        this.customer = {
+            id:1,
+            name:"John Doe",
+            email:"a@b.com"
+        }
     }
-    hasBirthday(){
-        this.age+=1;
-    }
-    showAge(){
-        return this.age;
-    }
+}
+interface Customer{
+    id:number, name:string, email:string
 }
