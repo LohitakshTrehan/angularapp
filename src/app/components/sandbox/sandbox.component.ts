2b390a6fec6a76cd0665f004fb606ec92e7220ca
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-06-15 05:26:48 +0530

    meh
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,69 +2,48 @@ import { Component } from '@angular/core';
 import { Customer } from './Customer';
 @Component({
     selector:'sandbox',
-    // template:`<h1>
-    //                 {{name}} is {{age}} yrs old
-    //           </h1>
-    //           <h2>
-    //                 his name is {{person.firstname}} {{person.lastname}}
-    //           </h2>
-    //           <p>{{showAge()}}</p>
-    // `
-    templateUrl:`./sandbox.component.html`,
-    styleUrls:['./sandbox.component.css']
+    template:`
+        <h1>Hello World</h1>
+        {{people}}
+        <ul>
+            <li *ngFor="let person of people">
+                {{person}}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people, let i = index">
+                {{i+1}} {{person}}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people2">
+                {{person.firstName}} {{person.lastName}}
+            </li>
+        </ul>
+    `
 })
 
 export class SandboxComponent{
-    // name:string = "alabama";
-    // age:number = 20;
-    // person = {firstname:"Steven", lastname:"Tyler"};
-    // hasChildren:boolean=true;
-    // city:any = "Boston";//can be any type
-    // myNumbersArray:number[] = [1,2,3];
-    // myStringsarray:string[] =["helo","world"];
-    // myAnyArray:any[] = [1,2,"Hello"];
-    // myTuple:[string, number]=["hello",3];
-    // unusable:void =undefined;
-    // u:undefined = undefined;
-    // n:null=null;
-    // s:null=undefined;
-    // constructor(){
-    //     console.log("running constructor");
-    //     //can directly initialise variable here,
-    //     //without even adding the property
-    //     //this.age=23;
-    //     this.hasBirthday();
-    // }
-    // hasBirthday(){
-    //     this.age+=1;
-    // }
-    // showAge(){
-    //     return this.age;
-    // }
-    // customer:Customer;
-    // customers:Customer[];
-    // constructor(){
-    //     this.customer = {
-    //         id:1,
-    //         name:"John Doe",
-    //         email:"a@b.com"
-    //     }
-    //     this.customers=[
-    //         {
-    //             id:1,
-    //             name:"John Doe",
-    //             email:"a@b.com"
-    //         },
-    //         {
-    //             id:2,
-    //             name:"Steven",
-    //             email:"b@c.com"
-    //         },
-    //         {
-    //             id:1,
-    //             name:"Jimmy",
-    //             email:"c@d.com"
-    //         } 
-    //     ]
-    // }
+    people=['Rick','Daryl','Carl','Glen'];
+    people2 = [
+        {
+            firstName:"Rick",
+            lastName:"Grime"
+        },
+        {
+            firstName:"Daryl",
+            lastName:"Dixon"
+        },
+        {
+            firstName:"Steven",
+            lastName:"Smith"
+        },
+        {
+            firstName:"Gwenn",
+            lastName:"Tenyson"
+        },
+    ]
+    constructor(){
+        this.people[2]='Carol';
+    }
 }
