2a3bd26fa623aa6b8521e0fc24c1670310b1001e
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-06-14 13:04:28 +0530

    a commit again1
    Signed-off-by: niranjan <niranjan@gmail.com>
    
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
 @Component({
     selector:'sandbox',
-    template:`<h1>Hello world</h1>`
+    template:`<h1>
+                    {{name}} is {{age}} yrs old
+              </h1>
+              <h2>
+                    his name is {{person.firstname}} {{person.lastname}}
+              </h2>
+              <p>{{showAge()}}</p>
+    `
 })
 
 export class SandboxComponent{
-    
+    name:string = "alabama";
+    age:number = 20;
+    person = {firstname:"Steven", lastname:"Tyler"};
+    hasChildren:boolean=true;
+    city:any = "Boston";//can be any type
+    myNumbersArray:number[] = [1,2,3];
+    myStringsarray:string[] =["helo","world"];
+    myAnyArray:any[] = [1,2,"Hello"];
+    myTuple:[string, number]=["hello",3];
+    unusable:void =undefined;
+    u:undefined = undefined;
+    n:null=null;
+    s:null=undefined;
+    constructor(){
+        console.log("running constructor");
+        //can directly initialise variable here,
+        //without even adding the property
+        //this.age=23;
+        this.hasBirthday();
+    }
+    hasBirthday(){
+        this.age+=1;
+    }
+    showAge(){
+        return this.age;
+    }
 }
