b1944535fde78e7b9cf5d7bb54a14970bccf43e3
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-06-14 11:14:03 +0530

    a commit again
    Signed-off-by: niranjan <niranjan@gmail.com>
    
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent{
-    
+    firstName = "JJ John";
+    lastName = "Smith";
+    age = 20;
+
+    constructor(){
+        console.log(this.age);
+        this.hasBirthday();
+        console.log(this.age);
+    }
+
+    hasBirthday(){
+        this.age+=1;
+    }
 }
