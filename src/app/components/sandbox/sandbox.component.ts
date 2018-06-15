0f7531b39fd7ab26d83441eed4ff1cc712de9cd9
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-06-15 20:12:29 +0530

    basic form submission
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -6,25 +6,27 @@ import { Customer } from './Customer';
         <h1>
             Hello World
         </h1>
-        <div class="container">
-            <form>
-                <div class="form-group">
-                    <label>Name</label>
-                    <input type="text" [(ngModel)]="name" name="name" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Age</label>
-                    <input type="number" [(ngModel)]="age" name="age" class="form-control">
-                </div>
-                <input type="submit" value="Submit" class="btn btn-success">
-            </form>
-            <h4> Name: {{name}} </h4>
-            <h4> Age: {{age}} </h4>
-        </div>
+        <form (submit)="onSubmit()">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" [(ngModel)]="name" name="name">
+            </div>
+            <input type="submit" class="btn btn-success" value="submit">
+        </form>
+        <hr>
+        <ul class="list-group">
+            <li class="list-group-item" *ngFor="let user of users">
+                {{user}}
+            </li>
+        </ul>
     `
 })
 // For using ngModel import in app.module
 export class SandboxComponent{
-    name:string = "";
-    age:number = 0;
+    name:string="";
+    users:string[]=["jon doe","Marie","alex"]
+    onSubmit(){
+        this.users.push(this.name);
+        this.name="";
+    }
 }
