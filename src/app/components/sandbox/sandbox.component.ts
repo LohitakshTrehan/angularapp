16d742026776679f4c5727f06af573ba21220446
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-06-15 10:57:06 +0530

    Property binding
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,29 +3,28 @@ import { Customer } from './Customer';
 @Component({
     selector:'sandbox',
     template:`
-        <h1>Hello
-            <!--
-            <span *ngIf="showName">{{name}}</span>
-            -->
-            <span *ngIf="showName;else noName">{{name}}</span>
-            <ng-template #noName>Ng Template</ng-template>
-
-            <p>Hello {{showName ? name: "world"}}</p>
-            
-            <hr>
-
-            <div [ngSwitch]="greeting">
-                <div *ngSwitchCase="'1'">Hello World</div>
-                <div *ngSwitchCase="'2'">Hello</div>
-                <div *ngSwitchCase="'3'">World</div>
-                <div *ngSwitchDefault>yo</div>
-            </div>
+        <h1>
+            Hello world
         </h1>
+        <!-- 3 ways to bind properties -->
+        <div>
+            <img src="{{ imageUrl }}"> <!-- can only use strings here -->
+        </div>
+        <div>
+            <img [src]="imageUrl"><!-- in this method we can use booleans and other stuff-->
+        </div>
+        <div>
+            <img bind-src="imageUrl">
+        </div>
+        <h4>Image Location: <span [textContent]="imageUrl"></span></h4>
+        <hr>
+        <h2> Create Post </h2>
+        <p [hidden]="isUnchanged"> post is changed, pls save it</p>
+        <button [disabled]="isUnchanged">Save It</button>
     `
 })
 
 export class SandboxComponent{
-    name:string = 'Jon Doe';
-    showName:boolean = false;
-    greeting:number=5;
+    imageUrl:string = "https://picsum.photos/200/100";
+    isUnchanged:boolean=false;
 }
