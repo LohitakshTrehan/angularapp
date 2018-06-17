0f91089d55faa76c0e65817947bacc5f7cdef973
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-06-17 16:03:50 +0530

    basic Observables
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Customer } from './Customer';
 import { DataService } from './../../services/data.services'
+
 @Component({
     selector:'sandbox',
     template:`
@@ -8,18 +9,20 @@ import { DataService } from './../../services/data.services'
             Hello World
         </h1>
         <ul class="list-group">
-            <li class="list-group-item" *ngFor="let user of users">
-                {{user}}
+            <li class="list-group-item" *ngFor="let d of data">
+                {{d}}
             </li>
         </ul>
     `
 })
 // For using ngModel import in app.module
 export class SandboxComponent{
+    data:any[]=[];
     //inject dependencies in constructor parameter
-    users:string[];
     constructor(public dataService:DataService){
-        console.log(this.dataService.getUsers());
-        this.users=this.dataService.getUsers();
+        this.dataService.getData().subscribe(data => {
+            console.log(data);
+            this.data.push(data);
+        })
     }
 }
--- src/app/services/data.services.ts
+++ src/app/services/data.services.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
 
 @Injectable()
 export class DataService{
-    users:string[];
+    //Observables are ES7 open streams
+    data:Observable<Array<number>>;
+
     constructor(){
-        this.users = ['Mark','Shanon','Beth','John'];
+
     }
-    getUsers(){
-        return this.users;
+
+    getData(){
+        this.data = new Observable(observer =>{
+            setTimeout(() =>{
+                observer.next(1);
+            },1000);
+            setTimeout(() =>{
+                observer.next(2);
+            },2000);
+            setTimeout(() =>{
+                observer.next("Hello");
+            },3000);
+            setTimeout(() =>{
+                observer.complete();
+            },4000);
+        });
+        return this.data; 
     }
 }
